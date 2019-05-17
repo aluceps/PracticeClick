69c3f3685cdcdcda115a3fbadf37ffb88dbb6672
Atsushi Takano <aluceps@gmail.com>
2019-05-17 15:55:51 +0900

    Add error case.

--- app/src/main/java/me/aluceps/MainActivity.kt
+++ app/src/main/java/me/aluceps/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Toast
 import me.aluceps.databinding.ActivityMainBinding
 import java.util.*
 
@@ -23,61 +24,60 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.message = message
-        CustomClickEvent(binding.shutter).setOnClickListener(object :
-            CustomClickEvent.OnClickListener {
-            override fun onClick() {
-                message.set("onClick")
-            }
 
-            override fun onLongClick(isFinished: Boolean) {
-                if (!isFinished) {
-                    message.set("onLongClickStart")
-                } else {
-                    message.set("onLongClickFinished")
+        CustomClickEvent(binding.shutter).apply {
+            setOnClickListener(object : CustomClickEvent.OnClickListener {
+                override fun onClick() {
+                    message.set("onClick")
                 }
-            }
-        })
+
+                override fun onLongClick(isFinished: Boolean) {
+                    if (!isFinished) {
+                        message.set("onLongClickStart")
+                    } else {
+                        message.set("onLongClickFinished")
+                    }
+                }
+
+                override fun onError(v: View, e: CustomClickEvent.TooShortClickTimeException) {
+                    runOnUiThread {
+                        Toast.makeText(v.context, e.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+            setCleanUp(Runnable {
+                binding.root.postDelayed({
+                    message.set("")
+                }, 1000)
+            })
+        }
     }
 
-    class CustomClickEvent(view: View, longDuration: Long = 500L) {
+    class CustomClickEvent(val view: View, val longDivision: Long = 500L, val longLowerLimit: Long = 1000L) {
+
+        class TooShortClickTimeException(private val time: Long) : Exception() {
+            override val message: String
+                get() = "You must be keep click longer than $time ms."
+        }
 
         private var isPressing = false
-        private var pressTime = 0L
 
-        private lateinit var timer: Timer
+        private var timer: Timer? = null
+
         private val handler = Handler()
+
+        private var cleanUpRunnable: Runnable? = null
 
         init {
             view.setOnTouchListener { _, e ->
                 when (e.action) {
                     MotionEvent.ACTION_DOWN -> {
                         isPressing = true
-                        pressTime = Date().time
-                        timer = Timer()
-                        timer.scheduleAtFixedRate(object : TimerTask() {
-                            override fun run() {
-                                val duration = Date().time - pressTime
-                                if (isPressing) {
-                                    if (duration < longDuration) {
-                                    } else {
-                                        listener?.onLongClick(false)
-                                    }
-                                } else {
-                                    if (duration < longDuration) {
-                                        listener?.onClick()
-                                    } else {
-                                        listener?.onLongClick(true)
-                                    }
-                                }
-                            }
-                        }, 10, 10)
+                        scheduledTimer()
                     }
                     MotionEvent.ACTION_UP -> {
                         isPressing = false
-                        handler.postDelayed({
-                            timer.cancel()
-                            timer.purge()
-                        }, 10)
+                        terminateTimer()
                     }
                     else -> Unit
                 }
@@ -85,9 +85,48 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        fun setCleanUp(r: Runnable) {
+            cleanUpRunnable = r
+        }
+
+        private fun scheduledTimer() {
+            val pressTime = Date().time
+            timer = Timer("CustomClickEventTimer")
+            timer?.scheduleAtFixedRate(object : TimerTask() {
+                override fun run() {
+                    val duration = Date().time - pressTime
+                    val isLong = duration < longDivision
+                    when (isPressing) {
+                        true -> if (isLong) {
+                        } else {
+                            listener?.onLongClick(false)
+                        }
+                        else -> if (isLong) {
+                            listener?.onClick()
+                        } else {
+                            if ((duration - longDivision) < longLowerLimit) {
+                                listener?.onError(view, TooShortClickTimeException(longLowerLimit))
+                            } else {
+                                listener?.onLongClick(true)
+                            }
+                        }
+                    }
+                }
+            }, 10, 10)
+        }
+
+        private fun terminateTimer() {
+            handler.postDelayed({
+                cleanUpRunnable?.run()
+                timer?.cancel()
+                timer?.purge()
+            }, 10)
+        }
+
         interface OnClickListener {
             fun onClick()
             fun onLongClick(isFinished: Boolean)
+            fun onError(v: View, e: TooShortClickTimeException)
         }
 
         private var listener: OnClickListener? = null
